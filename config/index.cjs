2ce681951a48b5adef87bde6c1e17c2cdc357181
Evan5201 <117964952+Evan5201@users.noreply.github.com>
2022-11-11 13:28:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-qap5mo-qvwcXvohUtlJxmbp5ag',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	vquKDyGaiC8v2vIdiPHz_6c31bTrWLTvCKfMOQl_4XM',
+      useTemplateId: 'LVaWO4qbFGvz89kgZGcPHX4oOPQXbYpVd2TGxKTKhqY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-14',
       festivals: [
