dd3747ca86b5bc63fbf7ef23362a51101ecbfb37
Evan5201 <117964952+Evan5201@users.noreply.github.com>
2022-11-11 13:33:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,7 +45,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2022-10-10' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2023-10-10' },
+        { keyword: 'marry_day', date: '2022-10-10' },
       ],
     },
   ],
@@ -56,7 +56,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '吕雨倬',
+      name: '',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-qap5uO7TnOKxWUiD5arnGWgd8w',
     }
